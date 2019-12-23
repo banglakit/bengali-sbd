8ec7ae010e6363939a7047d56a93bb9a43d4f954
Aniruddha Adhikary <aniruddha@saleswhale.com>
2019-12-23 17:59:03 +0800

    add complex test case

--- tests/test_common_sents.py
+++ tests/test_common_sents.py
@@ -1,3 +1,5 @@
+import pytest
+
 import bengali_sbd
 
 
@@ -9,5 +11,15 @@ def test_terminal_punct_at_end():
     assert bengali_sbd.split('আমি বাংলায় গান গাই।') == ['আমি বাংলায় গান গাই।']
 
 
-def test_terminal_punct_in_text():
-    assert bengali_sbd.split('আমি বাংলায় গান গাই। তুমি কই যাও?') == ['আমি বাংলায় গান গাই।', 'তুমি কই যাও?']
+@pytest.mark.parametrize('given,out', [
+    (
+            'আমি বাংলায় গান গাই। তুমি কই যাও?',
+            ['আমি বাংলায় গান গাই।', 'তুমি কই যাও?']
+    ),
+    (
+            'বাবা, তোমার সাথে যাব! আমার সাথে যাবে না তুমি? আমি রাগ করব।',
+            ['বাবা, তোমার সাথে যাব!', 'আমার সাথে যাবে না তুমি?', 'আমি রাগ করব।']
+    )
+])
+def test_terminal_punct_in_text(given, out):
+    assert bengali_sbd.split(given) == out
