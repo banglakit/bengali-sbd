6be52a62051a65245f0c9b8e0577712c45814041
Aniruddha Adhikary <aniruddha@saleswhale.com>
2019-12-23 17:59:35 +0800

    remove unused regex

--- bengali_sbd/splitter.py
+++ bengali_sbd/splitter.py
@@ -1,16 +1,8 @@
-import re
-
 from spacy.lang.bn import Bengali
 
 nlp = Bengali()
 nlp.add_pipe(nlp.create_pipe('sentencizer'))
-TERMINATING_PUNCT = re.compile(r'([^\u0980-\u09ff\s+])<=(।\?!)')
-
-
-def process_terminating_punct(text):
-    return TERMINATING_PUNCT.split(text)
 
 
 def split(text: str):
     return [s.text for s in nlp(text).sents]
-
